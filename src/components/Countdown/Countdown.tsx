6f42b16fafca9fa1c39e7292ddc06f4a4fb9b05d
Aidan Tilgner <aidantilgner02@gmail.com>
2023-01-28 22:45:31 -0800

    added some stuff

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,17 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Styles from "./Countdown.module.scss";
-import { getFormattedDateDifference } from "../../utils/dates";
+import { getDaysUntil } from "../../utils/dates";
 
 function Countdown({ date }: { date: Date }) {
   const [days, setDays] = useState<number>();
-  const [hours, setHours] = useState<number>();
-  const [minutes, setMinutes] = useState<number>();
 
   const updateCountdown = () => {
-    const diff = getFormattedDateDifference(date);
-    setDays(diff.days);
-    setHours(diff.hours);
-    setMinutes(diff.minutes);
+    const diff = getDaysUntil(date);
+    setDays(diff);
   };
 
   const check = setInterval(updateCountdown, 1000);
@@ -29,24 +25,6 @@ function Countdown({ date }: { date: Date }) {
         <p className={Styles.number}>{days}</p>
         <p className={Styles.type}>Days</p>
       </div>
-      <div className={Styles.item}>
-        <p className={Styles.number}>{hours}</p>
-        <p className={Styles.type}>Hours</p>
-      </div>
-      <div className={Styles.item}>
-        <p className={Styles.number}>{minutes}</p>
-        <p className={Styles.type}>Minutes</p>
-      </div>
-      <p
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          color: "red"
-        }}
-      >
-        Fix the time
-      </p>
     </div>
   );
 }
--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,24 +1,5 @@
-export const getDateDiff = (date: Date) => {
-  const now = new Date();
-  const diff = now.getTime() - date.getTime();
-
-  return new Date(diff);
-};
-
-export const getFlooredDate = (date: Date) => {
-  // whatever date that is passed, return 00:00:00 of that date
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-};
-
-export const getFormattedDateDifference = (date: Date) => {
-  const diff = getDateDiff(date);
-  const days = diff.getDate() - 1;
-  const hours = diff.getHours();
-  const minutes = diff.getMinutes();
-
-  return {
-    days,
-    hours,
-    minutes,
-  };
+export const getDaysUntil = (date: Date) => {
+  const today = new Date();
+  const diff = date.getTime() - today.getTime();
+  return Math.ceil(diff / (1000 * 60 * 60 * 24));
 };
