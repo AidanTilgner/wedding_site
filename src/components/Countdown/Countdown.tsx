6a29fb91cd2d29683b09711acb3ecb3033b409da
atilgner <aidantilgner02@gmail.com>
2023-05-16 14:59:27 -0700

    feat: added hours and minutes to countdown

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Styles from "./Countdown.module.scss";
-import { getDaysUntil } from "../../utils/dates";
 
 function Countdown({ date }: { date: Date }) {
   const [days, setDays] = useState<number>();
+  const [hours, setHours] = useState<number>();
+  const [minutes, setMinutes] = useState<number>();
 
   const updateCountdown = () => {
-    const diff = getDaysUntil(date);
-    setDays(diff);
+    const differenceInMilliseconds = date.getTime() - new Date().getTime();
+
+    const millisecondsInMinute = 1000 * 60;
+    const millisecondsInHour = millisecondsInMinute * 60;
+    const millisecondsInDay = millisecondsInHour * 24;
+
+    const days = Math.floor(differenceInMilliseconds / millisecondsInDay);
+    const hours = Math.floor(
+      (differenceInMilliseconds % millisecondsInDay) / millisecondsInHour
+    );
+    const minutes = Math.floor(
+      (differenceInMilliseconds % millisecondsInHour) / millisecondsInMinute
+    );
+    setDays(days);
+    setHours(hours);
+    setMinutes(minutes);
   };
 
   const check = setInterval(updateCountdown, 1000);
@@ -19,11 +34,24 @@ function Countdown({ date }: { date: Date }) {
     };
   }, []);
 
+  const getMinutesLanguage = () => {
+    // if minutes is plural, return "minutes", otherwise return "minute"
+    return minutes === 1 ? "Minute" : "Minutes";
+  };
+
   return (
     <div className={Styles.countdown}>
       <div className={Styles.item}>
         <p className={Styles.number}>{days}</p>
         <p className={Styles.type}>Days</p>
+      </div>
+      <div className={Styles.item}>
+        <p className={Styles.number}>{hours}</p>
+        <p className={Styles.type}>Hours</p>
+      </div>
+      <div className={Styles.item}>
+        <p className={Styles.number}>{minutes}</p>
+        <p className={Styles.type}>{getMinutesLanguage()}</p>
       </div>
     </div>
   );
